fc14704390043d6127769c17faa637bd85dcde60
mkanczuzewski <mkanczuzewski@gmail.com>
2022-01-03 09:41:07 -0500

    add category GET HTTP method

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,16 +1,51 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
-
 // The `/api/categories` endpoint
 
+// find all categories with associated Products
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  console.log('====================');
+  Category.findAll({
+    attributes: ['id', 'category_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['product_name']
+      }
+    ]
+  })
+  .then(dbPostData => res.json(dbPostData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
+  // find one category by its `id` value with associated Products
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'category_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['product_name']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });  
 });
 
 router.post('/', (req, res) => {
